81853d058ac0afbad681f3f291363852cf6c6f0a
James Alexander <60295314+Jalexander01@users.noreply.github.com>
2021-06-08 10:49:49 -0500

    JSX updating

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "James " + 7;
+const lastname = "Damia;n";
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1>
-    <p>El JXS es solo una herramienta mas {name}</p>
+    <h1>
+      Hello {name} {lastname}
+    </h1>
+    <p>El JXS es solo una herramienta mas {Math.floor(Math.random() * 3)} </p>
   </div>,
   document.getElementById("root")
 );
